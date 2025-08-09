449f594119852b2f58ef5760f33bce1e71cc0309
jaluiovilash <ovilashjalui@gmail.com>
2025-08-09 18:37:59 +0530

    Added source codes to my project

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -7,13 +7,15 @@ const Projects = () => {
       id: 1,
       image: project1,
       link: "https://apple-iphone15.web.app/",
+      code: "https://github.com/jaluiovilash/iphone15-website",
       title: "iPhone 15 Website Clone",
       description: `This is a clone of Apple’s iPhone 15 Pro website using React.js and TailwindCSS. It highlights the effective use of GSAP (Greensock Animations) and Three.js for displaying iPhone 15 Pro models in various colors and shapes.`
     },
     {
       id: 2,
       image: project2,
-      link: "https://github.com/jaluiovilash/supportdesk",
+      link: "https://jo-supportdesk.vercel.app/",
+      code: "https://github.com/jaluiovilash/supportdesk",
       title: "SupportDesk - A Ticketing System",
       description: `SupportDesk is a final year college project—a MERN stack ticketing system with role-based authentication, allowing users to create tickets and admins to manage, update, or delete them efficiently.`
     },
@@ -21,6 +23,7 @@ const Projects = () => {
       id: 3,
       image: project3,
       link: "https://github.com/jaluiovilash/RaktSetu-Backend.git",
+      code: "https://github.com/jaluiovilash/RaktSetu-Backend.git",
       title: "RaktSetu - Blood Bank Management System Backend",
       description: `RaktSetu backend, a Node.js-powered Blood Bank Management System, streamlines donor registrations, blood stock, hospital requests, and emergency notifications using Express and MongoDB for efficient inventory and request handling.`
     },
@@ -28,6 +31,7 @@ const Projects = () => {
       id: 4,
       image: coming_soon_grey,
       link: "https://colossalcodes.vercel.app/",
+      code: "https://github.com/jaluiovilash/colossalcodes",
       title: "ColossalCodes",
       description: `ColossalCodes is a hub for devs exploring TypeScript, Rust & future tech. Learn with tutorials, real-world projects & trend insights. Explore more on our upcoming site. Subscribe now and level up your coding game!`
     }
@@ -74,6 +78,14 @@ const Projects = () => {
                 className="text-xl"
               >
                 <VisitHere title="Explore Project" />
+              </a>
+              <a
+                href={project.code}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-md font-semibold text-orange-600 hover:text-white"
+              >
+                Source Code
               </a>
               <h2 className="text-xl font-medium py-4">{project.title}</h2>
               <p className="descp text-xl py-4">{project.description}</p>
