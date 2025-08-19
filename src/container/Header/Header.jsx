0c4aa6b2353bff1ffc49220f7f1755bb773cdf49
jaluiovilash <ovilashjalui@gmail.com>
2025-08-19 22:11:34 +0530

    changes on mobile UI

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -3,7 +3,7 @@ import { ShinyText } from "../../components/react-bits/index";
 const Header = () => {
   return (
     <div className="py-12 sm:py-16 md:py-20 lg:py-24 md:px-16 px-6 border-b">
-      <h1 className="font-bold font-montserrat text-3xl md:text-4xl lg:text-5xl xl:text-6xl mx-auto leading-tight md:w-4/5">
+      <h1 className="font-bold font-montserrat text-2xl md:text-4xl lg:text-5xl xl:text-6xl mx-auto leading-tight md:w-4/5">
         <ShinyText
           text="BUILDING INTELLIGENT INTERFACES AND SYSTEMS THAT SCALE WITH LASTING IMPACT"
           disabled={false}
